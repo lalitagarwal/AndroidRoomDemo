5b18aeb8f839d7d01c23a6ff2dff5b5f5b1ed3fb
Lalit <agarwal.lalit91@gmail.com>
2019-02-04 21:49:05 -0500

    Converted Artist and Track data class into a room entity.

--- app/src/main/java/android/room/androidroomdemo/entity/Artist.kt
+++ app/src/main/java/android/room/androidroomdemo/entity/Artist.kt
@@ -1,12 +1,23 @@
 package android.room.androidroomdemo.entity
 
-data class Artist(
-    var id: Int,
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "artist")
+data class Artist(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+
+    @ColumnInfo(name = "artist_name")
+    @NonNull
     var name: String,
 
+    @ColumnInfo(name = "artist_genre")
     var genre: String,
 
+    @ColumnInfo(name = "artist_country")
     var country: String
 ) {
     override fun toString(): String {
--- app/src/main/java/android/room/androidroomdemo/entity/Track.kt
+++ app/src/main/java/android/room/androidroomdemo/entity/Track.kt
@@ -1,10 +1,21 @@
 package android.room.androidroomdemo.entity
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "track")
 data class Track(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "track_id")
     var trackId: Int = 0,
 
+    @ColumnInfo(name = "track_name")
+    @NonNull
     var trackName: String,
 
+    @ColumnInfo(name = "track_duration")
     var duration: Long
 ) {
     override fun toString(): String {
